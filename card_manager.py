5f3ff66d5e5f1767aca3daddea8db3df43970c65
George Honeywell <george.honeywell@live.co.uk>
2025-07-12 18:01:06 +0100

    Columns in the treeview now have a fixed size depending on the value being displayed

--- card_manager.py
+++ card_manager.py
@@ -43,7 +43,7 @@ def add_card():
 def refresh_card_viewer():
     card_viewer.delete(*card_viewer.get_children())
     for row in conn.execute('SELECT * FROM card_db'):
-        card_viewer.insert("", 'end', values=(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]))            
+        card_viewer.insert("", 'end', values=(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]))
 
 # Card Labels
 label_card_mana = ttk.Label(tabOne, text="Mana:").grid(row=0, column=0)
@@ -84,17 +84,28 @@ button_add_card = ttk.Button(tabOne, text="Add Card", command=add_card).grid(row
 button_update_card_viewer = ttk.Button(tabTwo, text="Reload Database", command=refresh_card_viewer).pack()
 
 card_viewer = ttk.Treeview(tabTwo)
-card_viewer["columns"] = ("1","2","3","4","5","6","7","8","9")
+card_viewer["columns"] = ("#0","#1","#2","#3","#4","#5","#6","#7","#8")
 
-card_viewer.heading("1", text="Card ID")
-card_viewer.heading("2", text="Mana")
-card_viewer.heading("3", text="Title")
-card_viewer.heading("4", text="Type")
-card_viewer.heading("5", text="Subtype")
-card_viewer.heading("6", text="Effect")
-card_viewer.heading("7", text="Attack")
-card_viewer.heading("8", text="Affinity")
-card_viewer.heading("9", text="Health")
-card_viewer.pack()
+card_viewer.column('#0', width=1) # - Blank
+card_viewer.column('#1', width=50) # ID
+card_viewer.column('#2', width=50) # Mana
+card_viewer.column('#3', width=100) # Title
+card_viewer.column('#4', width=100) # Type
+card_viewer.column('#5', width=100) # Subtype
+card_viewer.column('#6', width=200) # Effect
+card_viewer.column('#7', width=50) # Attack
+card_viewer.column('#8', width=50) # Affinity
+card_viewer.column('#9', width=50) # Health
+
+card_viewer.heading("0", text="ID")
+card_viewer.heading("1", text="Mana")
+card_viewer.heading("2", text="Title")
+card_viewer.heading("3", text="Type")
+card_viewer.heading("4", text="Subtype")
+card_viewer.heading("5", text="Effect")
+card_viewer.heading("6", text="Attack")
+card_viewer.heading("7", text="Affinity")
+card_viewer.heading("8", text="Health")
+card_viewer.place(width=1200, height=250, y=30)
 
 window.mainloop()
